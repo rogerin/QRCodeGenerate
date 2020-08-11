3bdc578137541fd30f9222febcdc476e8ddeadea
Rogerio Alencar Filho <geriofilho@gmail.com>
2020-08-11 18:42:11 -0300

    deploy

---
+++ src/config/constants.js
@@ -0,0 +1,26 @@
+const constants = {
+    params: {
+        ecl: {
+            defaultValue: "L",
+            options: ['L', 'M', 'H', 'Q']
+        },
+        background: {
+            defaultValue: "white",
+            options: { message: 'ALL HEXA exemple #FF0000 '}
+        },
+        color: {
+            defaultValue: "#000000",
+            options: { message: 'ALL HEXA exemple #FF0000 '}
+        },
+        width: {
+            defaultValue: "200",
+            options: { message: 'Numbem in pixel '}
+        },
+        height: {
+            defaultValue: "200",
+            options: { message: 'Numbem in pixel '}
+        }
+    }
+}
+
+module.exports = constants;
--- src/routes/qrcode.js
+++ src/routes/qrcode.js
@@ -1,35 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var constants = require('../config/constants');
 
 var QRCode = require('qrcode-svg');
 
 
 /* GET users listing. */
 router.get('/helper', function(req, res, next) {
-    res.json({
-        params: {
-            ecl: {
-                defaultValue: "L",
-                options: ['L', 'M', 'H', 'Q']
-            },
-            background: {
-                defaultValue: "white",
-                options: { message: 'ALL HEXA exemple #FF0000 '}
-            },
-            color: {
-                defaultValue: "#000000",
-                options: { message: 'ALL HEXA exemple #FF0000 '}
-            },
-            width: {
-                defaultValue: "200",
-                options: { message: 'Numbem in pixel '}
-            },
-            height: {
-                defaultValue: "200",
-                options: { message: 'Numbem in pixel '}
-            }
-        }
-    })
+    res.json(constants.params)
 })
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -40,20 +18,57 @@ router.get('/', function(req, res, next) {
 
     const svg = new QRCode(
         {
-            content: url,
-            ecl: ecl || "L",
-            background: '#'+background || 'white',
-            color: '#'+color ||  '#000000',
-            width: width ||  '200',
-            height: height ||  '200',
-            
-            
-        } 
-    );
+            content: url, ecl: ecl || "L", background: '#'+background || 'white', 
+            color: '#'+color ||  '#000000', width: width ||  '200', height: height ||  '200'
+        });
     res.statusCode = 200;
     res.setHeader('Content-type', 'image/svg+xml')
     
     res.send(svg.svg())
 });
+
+router.get('/logo/:logo', async function(req, res) {
+    require('babel-polyfill');
+    var brandedQRCode = require('branded-qr-code');
+    var path = require("path"); 
+
+    const { url, ratio, background, color, width,height } = req.query
+    const { logo } = req.params
+    if(!url) {
+        res.json({error: 'URL faltando...'} )
+    }
+    if(!logo) {
+        res.json({error: 'Você deve escolher uma logo. [logo1, logo2, logoverde]'} )        
+    }
+
+    var file = path.resolve("./") + `/public/images/icons/${logo}.png`;
+    
+    // Return a buffer with the PNG of the code
+    const qr = await brandedQRCode.generate({ 
+        text: url, 
+        path: file,
+        ratio: ratio || 2,
+        ignoreCache: true,
+        opt: {
+            width: width || 200,
+            height: height || 200,
+            color: {
+                dark: color ? '#'+color : '#000000',
+                light: background ? '#'+background : '#ffffff' // Transparent background
+              }
+        }
+    });
+    
+    
+    brandedQRCode.route(res)
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'image/png')
+    res.send(qr);
+
+})
+
+
+
+    
 
 module.exports = router;
